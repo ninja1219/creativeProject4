cc7e29c50c8b3aeeca6014a7681c9b75cdca2dd0
Ryan Meyer <rmmeyer19@gmail.com>
2021-12-04 12:24:43 -0700

    Fixed api port to 3001

--- back-end/server.js
+++ back-end/server.js
@@ -65,4 +65,4 @@ app.delete('/api/items/:id', (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3001, () => console.log('Server listening on port 3001!'));
